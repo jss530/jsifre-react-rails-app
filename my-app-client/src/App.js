c60860ba30cb2272f7aca0f058f88f08cfc55942
jss530 <jsifre01@gmail.com>
2018-04-11 15:25:41 -0400

    Finished intro css and initial buttons

--- my-app-client/src/App.js
+++ my-app-client/src/App.js
@@ -10,14 +10,22 @@ class App extends Component {
             <h1>UGLY DELICIOUS</h1>
             <p>Because ugly food needs love, too.</p>
         </header>
+
         <div className="navbar">
           <p>NavBar</p>
         </div>
-        <div className="App-intro">
-           <p className="homepage-intro">Welcome to Ugly Delicious. We connect grocery stores looking to sell imperfect produce with
-           individuals and businesses looking to purchase them.</p>
-           <br/>
-           Get started or Log in
+
+        <div className="container">
+          <div className="App-intro">
+             <div className="homepage-intro">
+               <p>Welcome to Ugly Delicious. We connect grocery stores looking to sell imperfect produce with
+               individuals and businesses looking to purchase them.</p>
+               <br/>
+               <button type="button" id="intro-button">Get started</button>
+               <div class="divider"/>
+               <button type="button" id="intro-button">Log in</button>
+             </div>
+           </div>
         </div>
       </div>
     );
