5ea34ca910abe4ff6650c0225227297852e66946
jss530 <jsifre01@gmail.com>
2018-04-10 20:52:11 -0400

    Added more divs to homepage and added image

--- my-app-client/src/App.js
+++ my-app-client/src/App.js
@@ -10,10 +10,15 @@ class App extends Component {
             <h1>UGLY DELICIOUS</h1>
             <p>Because ugly food needs love, too.</p>
         </header>
-        <p className="App-intro">
-           Welcome to Ugly Delicious. We connect grocery stores looking to sell imperfect produce with
-           individuals and businesses looking to purchase them.
-        </p>
+        <div className="navbar">
+          <p>NavBar</p>
+        </div>
+        <div className="App-intro">
+           <p className="homepage-intro">Welcome to Ugly Delicious. We connect grocery stores looking to sell imperfect produce with
+           individuals and businesses looking to purchase them.</p>
+           <br/>
+           Get started or Log in
+        </div>
       </div>
     );
   }
