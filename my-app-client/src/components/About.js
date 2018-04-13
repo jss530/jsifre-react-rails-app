041f6cd5844746627dedde31c0cb09f5cad83414
jss530 <jsifre01@gmail.com>
2018-04-13 17:48:19 -0400

    Minor editorial changes to About page

--- my-app-client/src/components/About.js
+++ my-app-client/src/components/About.js
@@ -6,27 +6,27 @@ const About = () => {
   return (
       <div>
         <div className="title">
-          <h1>About Ugly Delicious</h1>
+          <h1>About Us</h1>
         </div>
           <div id="content">
             <div class="col">
               <img src={eggplants} alt={"eggplants"}/>
             </div>
             <div class="col">
-              <p>I created Ugly Delicious because I am passionate about food - and I hate to see it go to waste.</p>
+              <p>We created Ugly Delicious because we are passionate about food - and we hate to see it go to waste.</p>
               <p><strong>FACT: Did you know half of all produce in the U.S. is thrown away because
               of imperfections?</strong></p>
               <p>The demand for &ldquo;perfect&ldquo; food is so great that perfectly good food
               is getting sent to the trash. Not only does this take a toll on the environment, but it has impacts on
               the rates of poverty and hunger as well. </p>
 
-              <p>My mission is threefold:</p>
+              <p>Our mission is threefold:</p>
 
               <ol className="mission-statement">
                 <li>Change people&lsquo;s perceptions of &ldquo;ugly&ldquo; food</li><br/>
                 <li>Stop the waste before it occurs: connect grocery stores that would otherwise have to
                 trash their produce with individuals and businesses that want to buy them.</li><br/>
-                <li>As much as I can, help decrease the rate of hunger in our country.</li><br/>
+                <li>As much as we can, help decrease the rate of hunger in our country.</li><br/>
               </ol>
 
             </div>
