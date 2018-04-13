df9f553b75d483a9281a3c3a726efc6564b6557c
jss530 <jsifre01@gmail.com>
2018-04-12 20:53:04 -0400

    Finished about page and css

--- my-app-client/src/components/About.js
+++ my-app-client/src/components/About.js
@@ -4,32 +4,34 @@ import eggplants from '../details/img/eggplant-med.jpg'
 
 const About = () => {
   return (
-    <div className="title">
-      <h1>About Ugly Delicious</h1>
-        <div id="content">
-          <div class="col">
-            <img src={eggplants} alt={"eggplants"}/>
-          </div>
-          <div class="col">
-            <p>I created Ugly Delicious because I am passionate about food - and I hate to see it go to waste.</p>
-            <p><strong>FACT: Did you know half of all produce in the U.S. is thrown away because
-            of imperfections?</strong></p>
-            <p>The demand for &ldquo;perfect&ldquo; food is so great that perfectly good food
-            is getting sent to the trash. Not only does this take a toll on the environment, but it has impacts on
-            the rates of poverty and hunger as well. </p>
-
-            <p>My mission is threefold:</p>
-
-            <ol className="mission-statement">
-              <li>Change people&lsquo;s perceptions of &ldquo;ugly&ldquo; food</li><br/>
-              <li>Stop the waste before it occurs: connect grocery stores that would otherwise have to
-              trash their produce with individuals and businesses that want to buy them.</li><br/>
-              <li>As much as I can, help decrease the rate of hunger in our country.</li><br/>
-            </ol>
-
-          </div>
+      <div>
+        <div className="title">
+          <h1>About Ugly Delicious</h1>
         </div>
-    </div>
+          <div id="content">
+            <div class="col">
+              <img src={eggplants} alt={"eggplants"}/>
+            </div>
+            <div class="col">
+              <p>I created Ugly Delicious because I am passionate about food - and I hate to see it go to waste.</p>
+              <p><strong>FACT: Did you know half of all produce in the U.S. is thrown away because
+              of imperfections?</strong></p>
+              <p>The demand for &ldquo;perfect&ldquo; food is so great that perfectly good food
+              is getting sent to the trash. Not only does this take a toll on the environment, but it has impacts on
+              the rates of poverty and hunger as well. </p>
+
+              <p>My mission is threefold:</p>
+
+              <ol className="mission-statement">
+                <li>Change people&lsquo;s perceptions of &ldquo;ugly&ldquo; food</li><br/>
+                <li>Stop the waste before it occurs: connect grocery stores that would otherwise have to
+                trash their produce with individuals and businesses that want to buy them.</li><br/>
+                <li>As much as I can, help decrease the rate of hunger in our country.</li><br/>
+              </ol>
+
+            </div>
+          </div>
+      </div>
   );
 };
 
