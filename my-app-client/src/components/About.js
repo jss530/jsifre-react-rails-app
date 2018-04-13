a2d2091347f5546d45ecaba9ceaa13fff8579d0e
jss530 <jsifre01@gmail.com>
2018-04-12 20:01:29 -0400

    Started About.js page and corresponding CSS

---
+++ my-app-client/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="title">
+      <h1>About Us</h1>
+    </div>
+  );
+};
+
+export default About;
