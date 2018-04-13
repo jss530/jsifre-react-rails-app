e2c3541912b2aacc6539b9a02d652f27d73a6288
jss530 <jsifre01@gmail.com>
2018-04-13 17:52:58 -0400

    Created initial files and structure for purchasers and suppliers components

---
+++ my-app-client/src/components/Suppliers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Suppliers.css';
+
+const Suppliers = () => {
+  return (
+      <div>
+        <h1> Suppliers Page </h1>
+      </div>
+    )
+  }
+
+export default Suppliers;
