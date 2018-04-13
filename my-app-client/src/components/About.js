b6b6b5c655af03cd264fbe79342192d9b8f91082
jss530 <jsifre01@gmail.com>
2018-04-12 20:19:12 -0400

    Added image to About page

--- my-app-client/src/components/About.js
+++ my-app-client/src/components/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './About.css';
+import eggplants from '../details/img/eggplant-med.jpg'
 
 const About = () => {
   return (
     <div className="title">
       <h1>About Us</h1>
+        <div id="content">
+          <div class="col">
+            <img src={eggplants} alt={"eggplants"}/>
+          </div>
+          <div class="col">
+            <p>Example content</p>
+          </div>
+        </div>
     </div>
   );
 };
