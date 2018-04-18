9ea7ab75bd2157e6970a4462759c0f71f899e27e
jss530 <jsifre01@gmail.com>
2018-04-18 13:51:39 -0400

    Switched Suppliers and SuppliersList code

--- my-app-client/src/components/Suppliers.js
+++ my-app-client/src/components/Suppliers.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import SuppliersPage from '../containers/SuppliersPage';
+import supplierActions from '../actions/supplierActions';
+import SuppliersShow from '../containers/SuppliersShow';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Route, Switch } from 'react-router-dom';
+import * as actions from '../actions/supplierActions'
 
-const Suppliers = ({ suppliers }) => {
-  const renderSuppliers = suppliers.map(supplier =>
+let suppliers = [
+  {name: "ABC", location: "New York", id: 1},
+  {name: "EFG", location: "Boston", id: 2}
+]
+
+export default class Suppliers extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      suppliers: []
+    }
+  }
+
+  componentDidMount() {
+    this.props.fetchSuppliers().then(suppliers => this.setState({ suppliers }))
+  }
+
+  render() {
+    return(
       <div>
-        <Link to={`/suppliers/${supplier.id}`}>{supplier.name}</Link>
-        <p>Supplier location: {supplier.location}</p>
+        <SuppliersPage suppliers={suppliers}/>
+        <h3>Please select a supplier to see their available inventory.</h3>
       </div>
-  )
+    )}
+  }
 
-  return (
-    <div>
-      {renderSuppliers}
-    </div>
-  )
+  function mapStateToProps(state) {
+    return {
+      suppliers: state.suppliers
+    };
+  }
+
+  function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
 }
 
-export default Suppliers;
+
+connect(mapStateToProps, mapDispatchToProps)(Suppliers);
--- my-app-client/src/containers/SuppliersPage.js
+++ my-app-client/src/containers/SuppliersPage.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react';
-import Suppliers from '../components/Suppliers';
-import supplierActions from '../actions/supplierActions';
-import SuppliersShow from './SuppliersShow';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Route, Switch } from 'react-router-dom';
-import * as actions from '../actions/supplierActions'
+import React from 'react'
+import { Link } from 'react-router-dom';
 
-
-export class SuppliersPage extends Component {
-
-  componentDidMount() {
-    this.props.fetchSuppliers().then(suppliers => this.setState({ suppliers }))
+const SuppliersPage = (props) => {
+  function renderSuppliers() {
+    return props.suppliers.map(supplier => {
+      return (
+        <div>
+          <Link to={`/suppliers/${supplier.id}`}>{supplier.name}</Link>
+          <p>Supplier location: {supplier.location}</p>
+        </div>
+      )
+    })
   }
-
-  render() {
-    return(
-      <div>
-        <Suppliers/>
-        <h3>Please select a supplier to see their available inventory.</h3>
-      </div>
-    )}
-  }
-
-  function mapStateToProps(state) {
-    return {
-      suppliers: state.suppliers
-    };
-  }
-
-  function mapDispatchToProps(dispatch) {
-  return {actions: bindActionCreators(actions, dispatch)}
+  return (
+    <div>
+      {renderSuppliers()}
+    </div>
+  )
 }
 
-
-export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(SuppliersPage);
+export default SuppliersPage;
