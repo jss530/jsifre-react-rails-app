9498d9dee8c16e110746740f305a39951920886e
jss530 <jsifre01@gmail.com>
2018-04-19 19:19:34 -0400

    Added to CSS to About page

--- my-app-client/src/components/About.js
+++ my-app-client/src/components/About.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './About.css';
-import eggplants from '../details/img/eggplant-med.jpg'
+import carrots from '../details/img/carrots-med.jpg'
 
 const About = () => {
   return (
 
-      <div className="body">
+      <div className="border">
         <div className="title">
           <h1>About Us</h1>
         </div>
           <div id="content">
-            <div class="col">
-              <img src={eggplants} alt={"eggplants"}/>
+            <div className="col-1">
+              <img src={carrots} alt={"carrots"}/>
             </div>
-            <div class="col">
+            <div className="col-2">
               <p>We created Ugly Delicious because we are passionate about food - and we hate to see it go to waste.</p>
               <p><strong>FACT: Did you know half of all produce in the U.S. is thrown away because
               of imperfections?</strong></p>
@@ -21,7 +21,7 @@ const About = () => {
               is getting sent to the trash. Not only does this take a toll on the environment, but it has impacts on
               the rates of poverty and hunger as well. </p>
 
-              <p>Our mission is threefold:</p>
+              <p className="mission-intro">Our mission is threefold:</p>
 
               <ol className="mission-statement">
                 <li>Change people&lsquo;s perceptions of &ldquo;ugly&ldquo; food</li><br/>
