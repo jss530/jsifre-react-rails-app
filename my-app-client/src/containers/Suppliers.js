5de04db5885f77dd5f5ecf832a0d87bdb6f04655
jss530 <jsifre01@gmail.com>
2018-04-19 20:23:30 -0400

    Added image and more CSS to suppliers page

--- my-app-client/src/containers/Suppliers.js
+++ my-app-client/src/containers/Suppliers.js
@@ -4,8 +4,6 @@ import { connect } from 'react-redux';
 import SupplierCard from '../components/SupplierCard';
 import { getSuppliers } from '../actions/suppliers';
 import './Suppliers.css';
-import smallMedley from '../details/img/medley-small.jpg'
-
 
 class Suppliers extends Component {
 
@@ -16,11 +14,12 @@ class Suppliers extends Component {
   render() {
     return(
       <div className="supplier-border">
-        <div className="title-section">
-          <img src={smallMedley} alt={"smallMedley"} id="col-3"/>
-          <h1 className="title" id="col-4">Suppliers List</h1>
-        </div>
-        <h3>Please select a supplier to see their available inventory.</h3>
+          <div className="hero-image">
+            <div className="hero-text">
+              <h1>Suppliers List</h1>
+            </div>
+          </div>
+        <h3 className="supplier-instructions">Please select a supplier to see their available inventory.</h3>
         {this.props.suppliers.map(supplier => <SupplierCard key={supplier.id} supplier={supplier} />)}
       </div>
     );
