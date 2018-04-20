bf44894b3b62689c581ba512977cabb2a1c4d358
jss530 <jsifre01@gmail.com>
2018-04-20 19:25:43 -0400

    Edited CSS of about page

--- my-app-client/src/components/About.js
+++ my-app-client/src/components/About.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './About.css';
-import carrots from '../details/img/carrots-med.jpg'
 
 const About = () => {
   return (
 
-      <div className="border">
-        <div className="title">
-          <h1>About Us</h1>
-        </div>
-          <div id="content" className="clearfix">
-            <div className="col-1">
-              <img src={carrots} alt={"carrots"}/>
+      <div className="about-us">
+          <div className="hero-image">
+            <div className="hero-text">
+              <h1>About Us</h1>
             </div>
-            <div className="col-2">
+          </div>
+          <div id="content">
+            <div className="about-text">
               <p>We created Ugly Delicious because we are passionate about food - and we hate to see it go to waste.</p>
               <p><strong>FACT: Did you know half of all produce in the U.S. is thrown away because
               of imperfections?</strong></p>
