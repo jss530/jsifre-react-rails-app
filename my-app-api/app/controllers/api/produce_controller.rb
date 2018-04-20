3e76048d4820e8b7edb68ca4a9be75570191284f
jss530 <jsifre01@gmail.com>
2018-04-20 19:41:47 -0400

    Added create update and show actions to supplier and produce controllers

--- my-app-api/app/controllers/api/produce_controller.rb
+++ my-app-api/app/controllers/api/produce_controller.rb
@@ -4,4 +4,27 @@ class Api::ProduceController < ApplicationController
     @produce = Produce.all
     render json: @produce
   end
+
+  def create
+    product = Produce.new(produce_params)
+    if @roduct.save
+      render json: product
+    else
+      render json: { message: product.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: @product
+  end
+
+  private
+
+  def set_product
+    @product = Produce.find_by(id: params[:id])
+  end
+
+  def produce_params
+    params.require(:product).permit(:name, :quantity)
+  end
 end
--- my-app-api/app/controllers/api/suppliers_controller.rb
+++ my-app-api/app/controllers/api/suppliers_controller.rb
@@ -4,4 +4,27 @@ class Api::SuppliersController < ApplicationController
     @suppliers = Supplier.all
     render json: @suppliers
   end
+
+  def create
+    supplier = Supplier.new(supplier_params)
+    if supplier.save
+      render json: supplier
+    else
+      render json: { message: supplier.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: @supplier
+  end
+
+  private
+
+  def set_supplier
+    @supplier = Supplier.find_by(id: params[:id])
+  end
+
+  def supplier_params
+    params.require(:supplier).permit(:name, :location)
+  end
 end
