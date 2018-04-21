122fb6571ccc457c7aed90865e6ae3b361000e40
jss530 <jsifre01@gmail.com>
2018-04-21 17:47:42 -0400

    Finished supplier show page header

--- my-app-client/src/containers/SuppliersShow.js
+++ my-app-client/src/containers/SuppliersShow.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './SuppliersShow.css';
 
-const SuppliersShow = ({ supplier }) => {
-
-  return (
-    <div className="show-supplier">
-      <div className="hero-image">
-        <div className="hero-text">
-          <h1>{supplier.name}</h1>
-        </div>
+const SuppliersShow = ({ supplier }) =>
+  <div className="show-supplier">
+    <div className="hero-image">
+      <div className="hero-text">
+        <h2>{supplier.name}</h2>
+        <p>{supplier.location}</p>
       </div>
     </div>
-  )
-}
+  </div>;
+
 
 const mapStateToProps = (state, ownProps) => {
   const supplier = state.suppliers.find(supplier => supplier.id === +ownProps.match.params.supplierId)
