4c9f9e2cb661b2bd2127519540ee88bc462c8f59
jss530 <jsifre01@gmail.com>
2018-04-22 20:01:36 -0400

    FInished buyers css and html

--- my-app-client/src/components/Buyers.js
+++ my-app-client/src/components/Buyers.js
@@ -13,7 +13,9 @@ const Buyers = () => {
         <div className="buyers-intro">
           <p id="opening-statement">Are you an individual or business looking to buy imperfect produce?</p>
           <p id="opening-statement">Welcome!</p>
-          <p><strong>How it works:</strong> first, create an account if you haven't done so already.
+          <p className="instructions"><span className="how-to"><strong>How it works:</strong></span><br/>
+          <br/>
+          First, create an account if you haven't done so already.
           Then, go to the Suppliers page and check out what's available.
           Each item is priced by the pound. Add as much as you like to your cart.
           After you check out, choose your delivery date and time. Then, all you have to do is wait
