c69cb8676c3c709a6bd3ef13ccc8cf7b419df8eb
jss530 <jsifre01@gmail.com>
2018-05-05 14:12:17 -0400

    Started CSS on produce list page

--- my-app-client/src/components/ProduceCard.js
+++ my-app-client/src/components/ProduceCard.js
@@ -4,6 +4,9 @@ const ProduceCard = ({ produce }) => {
   return (
       <div key={produce.id} className="produce-card">
         <p>{produce.name}</p>
+        <p>Availability: {produce.quantity} pounds</p>
+        <p>Price: {produce.price} cents per pound</p>
+        <br/>
       </div>
   );
 }
