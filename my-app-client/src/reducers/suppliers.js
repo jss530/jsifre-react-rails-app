3f7adbcadb2f30764c09c483be05dbfd7f5106aa
jss530 <jsifre01@gmail.com>
2018-05-05 17:25:31 -0400

    More refactoring for these likes

--- my-app-client/src/reducers/suppliers.js
+++ my-app-client/src/reducers/suppliers.js
@@ -1,6 +1,5 @@
 function suppliersReducer (state = [], action) {
   let supplier;
-  let index;
 
   switch(action.type) {
     case 'GET_SUPPLIERS_SUCCESS':
@@ -10,16 +9,14 @@ function suppliersReducer (state = [], action) {
       return state.concat(action.supplier);
 
     case 'LIKE_SUPPLIER':
-      index = state.findIndex(supplier => supplier.id === action.supplierId);
-      supplier = state[index];
+      suppliers: state.suppliers.map( (supplier) => {
 
-      if (supplier.likes >= 0) {
-        return [
-          ...state.slice(0, index),
-          Object.assign({}, supplier, { likes: supplier.likes += 1 }),
-          ...state.slice(index + 1)
-        ];
-      }
+        if (supplier.id === action.supplierId) {
+           return supplier.likes += 1
+        }
+
+        return supplier;
+      });
 
     default:
       return state;
